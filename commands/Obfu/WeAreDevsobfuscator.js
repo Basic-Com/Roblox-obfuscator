6585e4cba040a4c33b019b63edb47d9af25e6892
Basic-Com <wilsonzayvia@gmail.com>
2025-03-28 00:48:54 +1300

    Update WeAreDevsobfuscator.js

--- commands/Obfu/WeAreDevsobfuscator.js
+++ commands/Obfu/WeAreDevsobfuscator.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const { AttachmentBuilder, EmbedBuilder } = require('discord.js');
 
-const blockedURLs = [
-  "grabify.link", "bmwforum.co", "catsnthing.com", "discordinvite.com",
-  "freegiftcards.co", "gaming-at-my.best", "google.docs.com",
-  "joinmy.site", "curiouscat.club", "leancoding.co",
-  "stopify.co", "starbucksisbadforyou.com"
-];
-
 module.exports = {
   name: "wearedevs-obfuscator",
   description: "Obfuscate a Lua script using the WeAreDevs API",
